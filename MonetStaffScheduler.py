f811ab87c9a38cdfd16cc94d40d81d700ca05f2d
brucecurcio <brucecurcio@gmail.com>
2018-07-30 23:13:27 -0700

    day two

--- MonetStaffScheduler.py
+++ MonetStaffScheduler.py
@@ -4,6 +4,7 @@ import requests
 import os
 from pprint import pprint
 import datetime
+from operator import itemgetter
 
 def getBearerToken():
     
@@ -52,19 +53,103 @@ def getSchedule(bearerToken):
     return (scheduleDict)
 
 
-def slSplit(fullSchedule):
+def slSplit(fullScheduleDict):
+
+    slSchedule = [] #initialize empty list
+
+    #pull out shift lead assignments from full schedule
+    for i in range(0, len(fullSchedule['Result'])):
+        if 'SHIFT LEAD' in fullSchedule['Result'][i]['workDescription']:
+            slSchedule.append(fullSchedule['Result'][i])
+
+    #reorder shift lead assignments chronologically
+    orderedSLSchedule = sorted(slSchedule, key=itemgetter('startUTC'))
+    #print(orderedSLSchedule)
+
+    return(orderedSLSchedule)   
 
 
-#def qmSplit(fullSchedule):
+def qmSplit(fullSchedule):
+    qMSchedule = [] #initialize empty list
+
+    #pull out QM assignments from full schedule
+    for i in range(0, len(fullSchedule['Result'])):
+        if 'Queue Monitor' in fullSchedule['Result'][i]['workDescription']:
+            qMSchedule.append(fullSchedule['Result'][i])
+        
+    #reorder QM assignments chronologically
+    orderedQMSchedule = sorted(qMSchedule, key=itemgetter('startUTC'))
+    #print(orderedQMSchedule)
+
+    return(orderedQMSchedule)
+
+def stbSplit(fullSchedule):
+    stbSchedule = [] #initialize empty list
+
+    #pull out STB assignments from full schedule
+    for i in range(0, len(fullSchedule['Result'])):
+        if 'Stand-By' in fullSchedule['Result'][i]['workDescription']:
+            stbSchedule.append(fullSchedule['Result'][i])
+        
+    #reorder STB assignments chronologically
+    orderedSTBSchedule = sorted(stbSchedule, key=itemgetter('startUTC'))
+    #print(orderedSTBSchedule)
+
+    return(orderedSTBSchedule)
 
 
-#def stbSplit(fullSchedule):
+def availableSplit(fullSchedule):
+    availableSchedule = [] #initialize empty list
+
+    #pull out Available assignments from full schedule
+    for i in range(0, len(fullSchedule['Result'])):
+        if 'Available' in fullSchedule['Result'][i]['workDescription']:
+            availableSchedule.append(fullSchedule['Result'][i])
+        
+    #reorder Available assignments chronologically
+    orderedAvailableSchedule = sorted(availableSchedule, key=itemgetter('startUTC'))
+    #print(orderedAvailableSchedule)
+
+    return(orderedAvailableSchedule)
 
 
-#def availableSplit(fullSchedule):
+def removeFields(theSchedule):
+    
+    #remove unneeded fields in schedule
+    fields = ('agentID','externalID','workgroupName','workDescription','exportCode','user1',
+       'user2','user3','utcOffset','totalDuration','personalDuration')
+    
+    for i in range(0, len(theSchedule)):
+        for key in fields:
+            if key in theSchedule[i]:
+                del theSchedule[i][key]
+                
+    return(theSchedule)
+        
 
 
-#def buildCSV(slSchedule, qmSchedule, stbSchedule, availableSchedule):
+def buildCSV(slSchedule, qmSchedule, stbSchedule, availableSchedule):
+    with open('StaffSchedule.csv', 'w', newline='') as f:  # Just use 'w' mode in 3.x
+        w = csv.DictWriter(f, slSchedule[0].keys())
+
+        w.writerow({'employeeFullName': 'SHIFTLEAD', 'startUTC':'','endUTC':''})
+        w.writeheader()
+        w.writerows(slSchedule)
+        
+        w.writerow({'employeeFullName': '', 'startUTC':'','endUTC':''}) #empty row spacer
+        w.writerow({'employeeFullName': 'QM', 'startUTC':'','endUTC':''}) #write role to row
+        w.writeheader()
+        w.writerows(qmSchedule)
+        
+        w.writerow({'employeeFullName': '', 'startUTC':'','endUTC':''}) #empty row spacer
+        w.writerow({'employeeFullName': 'STANDBY', 'startUTC':'','endUTC':''}) #write role to row
+        w.writeheader()
+        w.writerows(stbSchedule)
+
+        w.writerow({'employeeFullName': '', 'startUTC':'','endUTC':''}) #empty row spacer
+        w.writerow({'employeeFullName': 'Available', 'startUTC':'','endUTC':''}) #write role to row
+        w.writeheader()
+        w.writerows(availableSchedule)
 
 
 #def sendToGoogle(scheduleCSV):
@@ -73,11 +158,22 @@ def slSplit(fullSchedule):
 if __name__ == "__main__":
     bearerToken = getBearerToken() #get bearer token from Monet
     fullSchedule = getSchedule(bearerToken) #grab full schedule from Monet
-    slSchedule = slSplit(fullSchedule) #pull out an order ShiftLead schedule
-    #qmSchedule = qmSplit(fullSchedule) #pull out and order QM schedule
-    #stbSchedule = stbSplit(fullSchedule) #pull out and order TSE3 Standby schedule
-    #availableSchedule = availableSplit(fullSchedule) #pull our and order bullpen duties
+    
+    #extract schedule and reorder entries according to start time
+    slSchedule = slSplit(fullSchedule) 
+    qmSchedule = qmSplit(fullSchedule) 
+    stbSchedule = stbSplit(fullSchedule) 
+    availableSchedule = availableSplit(fullSchedule)
 
-    #scheduleCSV = buildCSV(slSchedule, qmSchedule, stbSchedule, availableSchedule)
+    #remove unneeded columns from schedule
+    slScheduleFiltered = removeFields(slSchedule)
+    qmScheduleFiltered = removeFields(qmSchedule)
+    stbScheduleFiltered = removeFields(stbSchedule)
+    availableScheduleFiltered = removeFields(availableSchedule)
+
+    #adjust timezone to PST/PDT and strip month/day/year from time - TBD
+    
+
+    scheduleCSV = buildCSV(slScheduleFiltered, qmScheduleFiltered, stbScheduleFiltered, availableScheduleFiltered)
     
     #sendToGoogle(scheduleCSV)
